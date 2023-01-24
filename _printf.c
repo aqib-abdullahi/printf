6d717c4c014c79e61f6cca46eb56952625b756cb
aqib-abdullahi <abdullahiaqib@gmail.com>
2023-01-24 13:08:21 -0800

    printf file

---
+++ _printf.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * _printf - a function that produces
+ * output according to a format
+ * @format: a character string
+ * Return: Number of characters printed
+ * except the null byte used to end output
+ * to strings
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list aug;
+	int n = 0;
+	int l = 0;
+	char buff[100] = {0};
+	char * st;
+
+	va_start(aug, format);
+	while (format && format[n])
+	{
+		if (format[n] == '%')
+		{
+			n++;
+			switch (format[n])
+			{
+				case 'c':buff[l] = (char)va_arg(aug, int);
+					 l++;
+					 break;
+				case 's':st = va_arg(aug, char*);
+					 strcpy(&buff[l], st);
+					 l += strlen(st);
+					 break;
+			}
+		}
+		else
+		{
+			buff[l] = format[n];
+			l++;
+		}
+		n++;
+	}
+	fwrite(buff, l, 1, stdout);
+	va_end(aug);
+	return (l);
+}
---
+++ main.h
@@ -0,0 +1,6 @@
+#ifndef _HEADER_
+#define _HEADER_
+#include <stdarg.h>
+int _printf(const char *format, ...);
+
+#endif
