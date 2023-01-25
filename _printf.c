2cf113d08e1b0c2905bfa7bac6c327c79d82d16a
aqib-abdullahi <abdullahiaqib@gmail.com>
2023-01-24 17:12:03 -0800

    renwed

--- _printf.c
+++ _printf.c
@@ -1,58 +1,119 @@
 #include "main.h"
-#include "_itoa.c"
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
+
+
+void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - a function that produces
- * output according to a format
- * @format: a character string
- * Return: Number of characters printed
- * except the null byte used to end output
- * to strings
+
+ * print_buffer - Prints the contents of the buffer if it exist
+
+ * @buffer: Array of chars
+
+ * @buff_ind: Index at which to add next char, represents the length.
+
+ */
+
+void print_buffer(char buffer[], int *buff_ind)
+
+{
+
+        if (*buff_ind > 0)
+
+                write(1, &buffer[0], *buff_ind);
+
+
+        *buff_ind = 0;
+
+}
+
+/**
+
+ * _printf - Printf function
+
+ * @format: format.
+
+ * Return: Printed chars.
+
  */
 
 int _printf(const char *format, ...)
+
 {
-	va_list aug;
-	int n = 0;
-	int l = 0;
-	char buff[100] = {0};
-	char * st;
-	char tmp[100];
 
-	va_start(aug, format);
+        int i, printed = 0, printed_chars = 0;
 
-	while (format && format[n])
-	{
-		if (format[n] == '%')
-		{
-			n++;
-			switch (format[n])
-			{
-				case 'c':buff[l] = (char)va_arg(aug, int);
-					 l++;
-					 break;
-				case 's':st = va_arg(aug, char*);
-					 strcpy(&buff[l], st);
-					 l += strlen(st);
-					 break;
-				case 'd':_itoa(va_arg(aug, int), tmp, 10);
-					 strcpy(&buff[l], tmp);
-					 l += strlen(tmp);
-					 break;
-			}
-		}
-		else
-		{
-			buff[l] = format[n];
-			l++;
-		}
-		n++;
-	}
-	fwrite(buff, l, 1, stdout);
-	va_end(aug);
-	return (l);
+        int flags, width, precision, size, buff_ind = 0;
+
+        va_list list;
+
+        char buffer[BUFF_SIZE];
+
+
+        if (format == NULL)
+
+                return (-1);
+
+
+        va_start(list, format);
+
+
+        for (i = 0; format && format[i] != '\0'; i++)
+
+        {
+
+                if (format[i] != '%')
+
+                {
+
+                        buffer[buff_ind++] = format[i];
+
+                        if (buff_ind == BUFF_SIZE)
+
+                                print_buffer(buffer, &buff_ind);
+
+                        /* write(1, &format[i], 1);*/
+
+                        printed_chars++;
+
+                }
+
+                else
+
+                {
+
+                        print_buffer(buffer, &buff_ind);
+
+                        flags = get_flags(format, &i);
+
+                        width = get_width(format, &i, list);
+
+                        precision = get_precision(format, &i, list);
+
+                        size = get_size(format, &i);
+
+                        ++i;
+
+                        printed = handle_print(format, &i, list, buffer,
+
+                                flags, width, precision, size);
+
+                        if (printed == -1)
+
+                                return (-1);
+
+                        printed_chars += printed;
+
+                }
+
+        }
+
+
+        print_buffer(buffer, &buff_ind);
+
+
+        va_end(list);
+
+
+        return (printed_chars);
+
 }
